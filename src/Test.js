975d0444f2770e4afc7bf71d79b2870af0eea9ea
bostwiek <bostwiek@gmail.com>
2020-05-27 01:53:56 -0600

    got sorting working for arrays and object arrays - still need to push this to Main

--- src/Test.js
+++ src/Test.js
@@ -5,8 +5,9 @@ class Test extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			sortby: 'id'
+			sortAsc: true
 		}
+		this.flipSortAsc = this.flipSortAsc.bind(this);
 	}
 
 	Data = [
@@ -16,37 +17,50 @@ class Test extends React.Component {
 		{id: '3', name: 'Three', sort: 1}
 	];
 
-	Numbers = [5, 6, 4, 7, 3, 8, 2, 9, 1, 10]
+
+	flipSortAsc = () => {
+		let flipSortAsc = !this.state.sortAsc;
+		this.setState({sortAsc: flipSortAsc})
+	}
 
 	render() {
-
-		const buttonClicked = () => {
-			
-			// toggle the sort state if by ID
-			let toggleSort;
-			(this.state.sortby == 'id') ? toggleSort = 'name' : toggleSort = 'id';
-			this.setState({sortby: toggleSort});
-
+		const Numbers = [5, 6, 4, 7, 3, 8, 2, 9, 1, 10];
 
 			// sorting standard array
-			this.Numbers.sort((a, b) => a - b)
+			(this.state.sortAsc === true) ? 
+				Numbers.sort((a, b) => a - b) :
+				Numbers.sort((a, b) => b - a);
 
 			// sorting array composed of objects
-			this.Data.sort((a, b) => (a.id + b.id) ? 1 : -1)
+			(this.state.sortAsc === true) ? 
+				this.Data.sort((a, b) => (a.id < b.id) ? 1 : -1) : 
+				this.Data.sort((a, b) => (b.id < a.id) ? 1 : -1)
 
-		}
+
 
 		// also want a toggle for Ascending to Descending ( ~`3`)~
 		
 		return(
 			<>
-				{this.Data[0].name}, {this.Data[1].name}, {this.Data[2].name}, {this.Data[3].name}
+				{this.Data[0].name},
+				{this.Data[1].name},
+				{this.Data[2].name},
+				{this.Data[3].name}
 				<br />
-				{this.Numbers[0]}, {this.Numbers[1]}, {this.Numbers[2]}, {this.Numbers[3]}
+				{Numbers[0]},
+				{Numbers[1]},
+				{Numbers[2]},
+				{Numbers[3]},
+				{Numbers[4]},
+				{Numbers[5]},
+				{Numbers[6]},
+				{Numbers[7]},
+				{Numbers[8]},
+				{Numbers[9]}
 				<br />
 				{this.state.sortby}
 				<br />
-				<button onClick={buttonClicked}>Press me</button>
+				<button onClick={this.flipSortAsc}>Press me</button>
 			</>
 		)
 	}
