1b57fd4e88aa9df7d9076223c3ff950bdd890ff9
bostwiek <bostwiek@gmail.com>
2020-06-25 12:28:19 -0600

    remove animations

--- src/Main.js
+++ src/Main.js
@@ -11,8 +11,6 @@ import food from './food';
 import logo from './images/icons/crockpot.png';
 import background from './images/bg/4.png';
 
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-
 const bgStyle = {
 	backgroundImage: `url(${background})`
 }
@@ -144,8 +142,6 @@ class Main extends React.Component {
 
 					<div className="card-container">
 
-						<TransitionGroup className="card-animation-container">
-
 							{Object.keys(food).map(
 									(key, value) => {
 
@@ -160,24 +156,18 @@ class Main extends React.Component {
 												// Contains Meat
 												if(food[key].ismeat === true){
 													return(
-														<CSSTransition
+														<Card
+															foodName={food[key].name}
+															foodImg={food[key].img}
+															foodHp={food[key].hp}
+															foodHunger={food[key].hunger}
+															foodSanity={food[key].sanity}
+															foodRot={food[key].rot}
+															foodRecipes={food[key].recipes}
+															foodRestrictions={food[key].restrictions}
+															foodIsMeat={food[key].ismeat}
 															key={key}
-															timeout={500}
-															classNames="card"
-														>
-															<Card
-																foodName={food[key].name}
-																foodImg={food[key].img}
-																foodHp={food[key].hp}
-																foodHunger={food[key].hunger}
-																foodSanity={food[key].sanity}
-																foodRot={food[key].rot}
-																foodRecipes={food[key].recipes}
-																foodRestrictions={food[key].restrictions}
-																foodIsMeat={food[key].ismeat}
-																key={key}
-															/>
-														</CSSTransition>
+														/>
 													)
 												}
 
@@ -185,24 +175,18 @@ class Main extends React.Component {
 												// Does not contain meat
 												if(food[key].ismeat === false){
 													return(
-														<CSSTransition
+														<Card
+															foodName={food[key].name}
+															foodImg={food[key].img}
+															foodHp={food[key].hp}
+															foodHunger={food[key].hunger}
+															foodSanity={food[key].sanity}
+															foodRot={food[key].rot}
+															foodRecipes={food[key].recipes}
+															foodRestrictions={food[key].restrictions}
+															foodIsMeat={food[key].ismeat}
 															key={key}
-															timeout={500}
-															classNames="card"
-														>
-															<Card
-																foodName={food[key].name}
-																foodImg={food[key].img}
-																foodHp={food[key].hp}
-																foodHunger={food[key].hunger}
-																foodSanity={food[key].sanity}
-																foodRot={food[key].rot}
-																foodRecipes={food[key].recipes}
-																foodRestrictions={food[key].restrictions}
-																foodIsMeat={food[key].ismeat}
-																key={key}
-															/>
-														</CSSTransition>
+														/>
 													)
 												}
 											}
@@ -210,31 +194,23 @@ class Main extends React.Component {
 										} else {
 											// Both meat and non-meat foods
 											return(
-												<CSSTransition
+												<Card
+													foodName={food[key].name}
+													foodImg={food[key].img}
+													foodHp={food[key].hp}
+													foodHunger={food[key].hunger}
+													foodSanity={food[key].sanity}
+													foodRot={food[key].rot}
+													foodRecipes={food[key].recipes}
+													foodRestrictions={food[key].restrictions}
+													foodIsMeat={food[key].ismeat}
 													key={key}
-													timeout={500}
-													classNames="card"
-												>
-													<Card
-														foodName={food[key].name}
-														foodImg={food[key].img}
-														foodHp={food[key].hp}
-														foodHunger={food[key].hunger}
-														foodSanity={food[key].sanity}
-														foodRot={food[key].rot}
-														foodRecipes={food[key].recipes}
-														foodRestrictions={food[key].restrictions}
-														foodIsMeat={food[key].ismeat}
-														key={key}
-													/>
-												</CSSTransition>
+												/>
 											)
 										}
 									}
 								}
 							)}
-
-						</TransitionGroup>
 
 					</div>
 				</div>
