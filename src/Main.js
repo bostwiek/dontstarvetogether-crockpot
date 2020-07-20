59ef66add04e7289d5497e3ebb429bff899ca64f
bostwiek <bostwiek@users.noreply.github.com>
2020-07-20 17:23:30 -0600

    rename app to Crockpot Recipes

--- src/Main.js
+++ src/Main.js
@@ -180,7 +180,7 @@ class Main extends React.Component {
 				<header>
 					<div className="logo">
 						<span className="logo-subtext">Don't Starve Together</span>
-						<span className="logo-text">All Things Crockpot</span>
+						<span className="logo-text">Crockpot Recipes</span>
 						<img src={logo} alt='Crockpot Logo' />
 					</div>
 				</header>
